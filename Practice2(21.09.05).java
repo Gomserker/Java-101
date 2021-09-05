a133bca4ee186c6b55ecfac5d635eefe738cafd3
Byeong Gwan Seo <89896745+Gomserker@users.noreply.github.com>
2021-09-05 20:30:20 +0900

    Update Practice2(21.09.05).java

--- Practice2(21.09.05).java
+++ Practice2(21.09.05).java
@@ -33,7 +33,21 @@ public class Test1 {
 		else {
 			System.out.printf("%d에서 %d를 뺀 값은 %d 입니다.", y, x, y-x);
 		}
-				
-				
+
+		
+		//if-else가 아닌 삼항 연산자를 사용했을때.
+//		int x, y, resSmaller, resBigger;
+//		
+//		System.out.print("첫 번째 정수를 입력하시오: ");
+//		x = input.nextInt();
+//		
+//		System.out.print("두 번째 정수를 입력하시오: ");
+//		y = input.nextInt();
+//		
+//		resBigger = (x > y) ? x : y; // x보다 y가 큰 지를 보고, 크다면 x를 출력, y가 크다면 y를 출력(둘 중 큰걸 뱉어냄)
+//		resSmaller = (x > y) ? y : x; // x보다 y가 큰 지를 보고, 크다면 y를 출력, 작다면 x를 출력 (둘 중 작은걸 뱉어냄) 
+//		
+//		System.out.println(resBigger + "에서 " + resSmaller + "을 뺀 값은 " + (resBigger - resSmaller) + "입니다.");
+		
 	}
 }
