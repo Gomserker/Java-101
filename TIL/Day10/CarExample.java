322525548227ee19edb7c642163a95437797e316
Byeong Gwan Seo <89896745+Gomserker@users.noreply.github.com>
2021-09-14 22:57:05 +0900

    Update CarExample.java

--- TIL/Day10/CarExample.java
+++ TIL/Day10/CarExample.java
@@ -10,6 +10,20 @@ public class CarExample {
       myCar.run(); // Car 클래스의 run() 메소드 호출
     }
     
+    /*
+     boolean isLeftGas() {
+    if(gas==0) {
+      System.out.println("gas가 없습니다.");
+      return false;
+    }
+    System.out.println("gas가 있습니다.");
+    return true;
+  }
+  
+  isLeftGas() 메소드는 boolean 타입의 메소드이다.
+  */
+    
+    
     if(myCar.isLeftGas()) {
       System.out.println("gas를 주입할 필요가 없습니다.");
     } else {
