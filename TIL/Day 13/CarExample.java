04161b3790fd2395ef6902b7919c2d465ca1316a
Byeong Gwan Seo <89896745+Gomserker@users.noreply.github.com>
2021-09-17 23:18:53 +0900

    Update CarExample.java

--- TIL/Day 13/CarExample.java
+++ TIL/Day 13/CarExample.java
@@ -1,21 +1,21 @@
 public class CarExample {
  public static void main(String[] args) {
-  Car car1 = new Car();
+  Car car1 = new Car(); //①번 생성자로 생성된 Car 객체
   System.out.println("car1.company: " + car1.company);
   System.out.println();
 
-  Car car2 = new Car("Avante");
+  Car car2 = new Car("Avante"); //②번 생성자로 생성된 Car 객체
   System.out.println("car2.company: " + car2.company);
   System.out.println("car2.model: " + car2.model);
   System.out.println();
 
-  Car car3 = new Car("Grandeur", "Red");
+  Car car3 = new Car("Grandeur", "Red"); //③번 생성자로 생성된 Car 객체
   System.out.println("car3.company: " + car3.company);
   System.out.println("car3.model: " + car3.model);
   System.out.println("car3.color: " + car3.color);
   System.out.println();
 
-  Car car4 = new Car("Sonata", "Silver", 200);
+  Car car4 = new Car("Sonata", "Silver", 200); //④번 생성자로 생성된 Car 객체
   System.out.println("car4.company: " + car4.company);
   System.out.println("car4.model: " + car4.model);
   System.out.println("car4.color: " + car4.color);
